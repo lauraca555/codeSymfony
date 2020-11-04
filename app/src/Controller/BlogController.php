a270181dab6231ed7b4cb23b609fa7a67983ca5a
Laura Carazo <laura@self-couture.com>
2020-11-04 18:44:23 +0100

    code exemple

---
+++ app/src/Controller/BlogController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Articles;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog/", name="blog")
+     */
+    public function index(): Response
+    {   
+        $repo = $this->getDoctrine()->getRepository(Articles::class);
+        $articles = $repo->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+    /**
+     *@Route("/", name= "home")
+     */
+    public function home() {
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenido Pecador !!!!"
+        ]);
+    }
+    /**
+     *@Route("/blog/{id}", name= "blog_show")
+     */
+        
+    public function show($id) {
+        $repo = $this->getDoctrine()->getRepository(Articles::class);
+        $article = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+                'article' => $article
+        ]);
+    }
+}
